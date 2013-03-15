16a4c2c00a49e84c3e5dc6ae2997464a826a8f1f
Markus Bernhardt <markus.bernhardt@me.com>
2013-03-15 17:13:25 +0100

    Proxy

--- src/main/java/com/github/markusbernhardt/selenium2library/keywords/Selenium2LibraryEnhancement.java
+++ src/main/java/com/github/markusbernhardt/selenium2library/keywords/Selenium2LibraryEnhancement.java
@@ -1,12 +1,28 @@
 package com.github.markusbernhardt.selenium2library.keywords;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.NTCredentials;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.HttpCommandExecutor;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import com.github.markusbernhardt.selenium2library.Selenium2LibraryFatalException;
 import com.github.markusbernhardt.selenium2library.Selenium2LibraryNonFatalException;
 
 public class Selenium2LibraryEnhancement extends Waiting {
+
+	private static final int STRIPPED_HTTP_PROXY_PROTOCOL = 0;
+	private static final int STRIPPED_HTTP_PROXY_HOST = 1;
+	private static final int STRIPPED_HTTP_PROXY_PORT = 2;
+	private static final int STRIPPED_HTTP_PROXY_USER = 3;
+	private static final int STRIPPED_HTTP_PROXY_PASSWORD = 4;
 
 	// ##############################
 	// Keywords
@@ -434,6 +450,123 @@ public class Selenium2LibraryEnhancement extends Waiting {
 		}
 	}
 
+	public void setRemoteWebDriverProxy(String host, String port) {
+		setRemoteWebDriverProxy(host, port, "", "", "", "");
+	}
+
+	public void setRemoteWebDriverProxy(String host, String port, String user,
+			String password) {
+		setRemoteWebDriverProxy(host, port, user, password, "", "");
+	}
+
+	public void setRemoteWebDriverProxy(String host, String port, String user,
+			String password, String domain) {
+		setRemoteWebDriverProxy(host, port, user, password, domain, "");
+	}
+
+	public void setRemoteWebDriverProxy(String host, String port, String user,
+			String password, String domain, String workstation) {
+		if (host.length() == 0 || port.length() == 0) {
+			// No host and port given as proxy
+			return;
+		}
+		WebDriver webDriver = webDriverCache.getCurrent();
+		if (!(webDriver instanceof RemoteWebDriver)) {
+			// No reason to set a proxy for a non RemoteWebDriver
+			return;
+		}
+
+		String[] strippedHttpProxy = null;
+		String httpProxy = System.getenv().get("http_proxy");
+		if (httpProxy != null) {
+			strippedHttpProxy = stripHttpProxyEnvironmentVariable(httpProxy);
+		} else {
+			httpProxy = System.getenv().get("HTTP_PROXY");
+			if (httpProxy != null) {
+				strippedHttpProxy = stripHttpProxyEnvironmentVariable(httpProxy);
+			}
+		}
+
+		if (user.length() == 0) {
+			// look for a username
+			user = System.getProperty("http.proxyUser", "");
+			if (user.length() == 0) {
+				if (strippedHttpProxy != null
+						&& strippedHttpProxy[STRIPPED_HTTP_PROXY_HOST]
+								.equals(host)
+						&& strippedHttpProxy[STRIPPED_HTTP_PROXY_PORT]
+								.equals(port)) {
+					user = strippedHttpProxy[STRIPPED_HTTP_PROXY_USER];
+				}
+			}
+		}
+
+		if (password.length() == 0) {
+			// look for a password
+			password = System.getProperty("http.proxyPassword", "");
+			if (password.length() == 0) {
+				if (strippedHttpProxy != null
+						&& strippedHttpProxy[STRIPPED_HTTP_PROXY_HOST]
+								.equals(host)
+						&& strippedHttpProxy[STRIPPED_HTTP_PROXY_PORT]
+								.equals(port)
+						&& strippedHttpProxy[STRIPPED_HTTP_PROXY_USER]
+								.equals(user)) {
+					password = strippedHttpProxy[STRIPPED_HTTP_PROXY_PASSWORD];
+				}
+			}
+		}
+
+		RemoteWebDriver remoteWebDriver = (RemoteWebDriver) webDriver;
+		String fieldName = "<unknown>";
+		String className = "<unknown>";
+		try {
+			fieldName = "executor";
+			className = "HttpCommandExecutor";
+			Field field = RemoteWebDriver.class.getDeclaredField(fieldName);
+			field.setAccessible(true);
+			HttpCommandExecutor executor = (HttpCommandExecutor) field
+					.get(remoteWebDriver);
+
+			fieldName = "client";
+			className = "DefaultHttpClient";
+			field = HttpCommandExecutor.class.getDeclaredField(fieldName);
+			field.setAccessible(true);
+			DefaultHttpClient client = (DefaultHttpClient) field.get(executor);
+
+			AuthScope authScope = new AuthScope(host, Integer.parseInt(port));
+			client.getCredentialsProvider().setCredentials(authScope,
+					new NTCredentials(user, password, workstation, domain));
+
+			HttpHost proxy = new HttpHost(host, Integer.parseInt(port));
+			client.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY,
+					proxy);
+		} catch (SecurityException e) {
+			throw new Selenium2LibraryFatalException(
+					String.format(
+							"The SecurityManager does not allow us to lookup to the %s field.",
+							fieldName));
+		} catch (NoSuchFieldException e) {
+			throw new Selenium2LibraryFatalException(
+					String.format(
+							"The RemoteWebDriver dose not declare the %s field any more.",
+							fieldName));
+		} catch (IllegalArgumentException e) {
+			throw new Selenium2LibraryFatalException(String.format(
+					"The field %s does not belong to the given object.",
+					fieldName));
+		} catch (IllegalAccessException e) {
+			throw new Selenium2LibraryFatalException(
+					String.format(
+							"The SecurityManager does not allow us to access to the %s field.",
+							fieldName));
+		} catch (ClassCastException e) {
+			throw new Selenium2LibraryFatalException(
+					String.format("The %s field does not contain a %s.",
+							fieldName, className));
+		}
+	}
+
 	// ##############################
 	// Internal Methods
 	// ##############################
@@ -454,6 +587,50 @@ public class Selenium2LibraryEnhancement extends Waiting {
 		}
 		WebElement element = elements.get(0);
 		return element.isSelected();
+	}
+
+	protected String[] stripHttpProxyEnvironmentVariable(String httpProxy) {
+		String[] result = new String[5];
+
+		int index = httpProxy.indexOf("://");
+		if (index == -1) {
+			return null;
+		}
+		result[STRIPPED_HTTP_PROXY_PROTOCOL] = httpProxy.substring(0, index);
+		httpProxy = httpProxy.substring(index + 3);
+
+		result[STRIPPED_HTTP_PROXY_USER] = "";
+		result[STRIPPED_HTTP_PROXY_PASSWORD] = "";
+		index = httpProxy.indexOf("@");
+		if (index != -1) {
+			String usernamePassword = httpProxy.substring(0, index);
+			httpProxy = httpProxy.substring(index + 1);
+
+			index = usernamePassword.indexOf(":");
+			if (index == -1) {
+				result[STRIPPED_HTTP_PROXY_USER] = usernamePassword;
+			} else {
+				result[STRIPPED_HTTP_PROXY_USER] = usernamePassword.substring(
+						0, index);
+				result[STRIPPED_HTTP_PROXY_PASSWORD] = usernamePassword
+						.substring(index + 1);
+			}
+		}
+
+		index = httpProxy.indexOf(":");
+		if (index == -1) {
+			return null;
+		}
+		result[STRIPPED_HTTP_PROXY_HOST] = httpProxy.substring(0, index);
+		httpProxy = httpProxy.substring(index + 1);
+
+		index = httpProxy.indexOf("/");
+		if (index != -1) {
+			httpProxy = httpProxy.substring(0, index);
+		}
+		result[STRIPPED_HTTP_PROXY_PORT] = httpProxy;
+
+		return result;
 	}
 
 }
