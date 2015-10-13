c4d8a1dc65846f4b623daba56231c58b682e4f70
Juho Saarinen <juho.saarinen@gmail.com>
2015-10-13 22:27:45 +0300

    Update BrowserManagement.java
    Removed extra comments

--- src/main/java/com/github/markusbernhardt/selenium2library/keywords/BrowserManagement.java
+++ src/main/java/com/github/markusbernhardt/selenium2library/keywords/BrowserManagement.java
@@ -192,31 +192,6 @@ public class BrowserManagement extends RunOnFailureKeywordsAdapter {
 		}
 	}
 
-//	@RobotKeywordOverload
-//	public String openBrowser(String url) throws Throwable {
-//		return openBrowser(url, "firefox");
-//	}
-//
-//	@RobotKeywordOverload
-//	public String openBrowser(String url, String browserName) throws Throwable {
-//		return openBrowser(url, browserName, null);
-//	}
-//
-//	@RobotKeywordOverload
-//	public String openBrowser(String url, String browserName, String alias) throws Throwable {
-//		return openBrowser(url, browserName, alias, null);
-//	}
-//
-//	@RobotKeywordOverload
-//	public String openBrowser(String url, String browserName, String alias, String remoteUrl) throws Throwable {
-//		return openBrowser(url, browserName, alias, remoteUrl, null);
-//	}
-//
-//	public String openBrowser(String url, String browserName, String alias, String remoteUrl, String desiredCapabilities)
-//			throws Throwable {
-//		return openBrowser(url, browserName, alias, remoteUrl, desiredCapabilities, null);
-//	}
-
 	/**
 	 * Opens a new browser instance to given URL.<br>
 	 * <br>
